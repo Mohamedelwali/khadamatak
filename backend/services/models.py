f3aa3252f30bf20ec152927553628de7b199056e
ReemShoman <remshoman8@gmail.com>
2025-08-12 14:09:16 +0300

    create models

--- backend/services/models.py
+++ backend/services/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Service(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    provider = models.ForeignKey(User, on_delete=models.CASCADE, related_name='services')
+
+    def __str__(self):
+        return self.name

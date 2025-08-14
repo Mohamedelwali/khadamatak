1327363bc8d0c7d587bc04252fe02e5d48597af0
Mohamed Elwaly <mnoaman162@gmail.com>
2025-08-14 10:48:31 +0300

    Update new version of services app and reviews app

--- backend/services/serializers.py
+++ backend/services/serializers.py
@@ -2,13 +2,14 @@ from rest_framework import serializers
 from .models import Service, ServiceCategory
 from reviews.serializers import ReviewSerializer
 
-
+# ---------- Categories ----------
 class ServiceCategorySerializer(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M", read_only=True)
     class Meta:
         model = ServiceCategory
-        fields = "__all__"
+        fields = ["id", "name", "created_at"]  
 
-
+# ---------- Services ----------
 class ServiceSerializer(serializers.ModelSerializer):
     category = ServiceCategorySerializer(read_only=True)
     category_id = serializers.PrimaryKeyRelatedField(
@@ -18,16 +19,21 @@ class ServiceSerializer(serializers.ModelSerializer):
     )
     rating_avg = serializers.FloatField(read_only=True)
     rating_count = serializers.IntegerField(read_only=True)
+    currency = serializers.SerializerMethodField()  
+    created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M", read_only=True)  
 
     class Meta:
         model = Service
         fields = [
             "id", "title", "description", "provider_name", "category",
-            "category_id", "city", "price", "is_active", "created_at",
-            "rating_avg", "rating_count"
+            "category_id", "city", "price", "currency", "is_active",
+            "created_at", "rating_avg", "rating_count"
         ]
 
+    def get_currency(self, obj):
+        return "EGP"
 
+# ---------- Service Details with Reviews ----------
 class ServiceDetailSerializer(ServiceSerializer):
     reviews = ReviewSerializer(many=True, read_only=True)
 
